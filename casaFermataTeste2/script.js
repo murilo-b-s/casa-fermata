f34a3cf2b249148aff1a70fb1467c52a6a0dad3b
Murilo Brum Sena <murilobrumsena08@gmail.com>
2025-02-14 16:24:30 -0300

    responsivo celular

--- casaFermataTeste2/script.js
+++ casaFermataTeste2/script.js
@@ -2,6 +2,6 @@ document.getElementById('btn').
 addEventListener('click', function() {
     this.classList.toggle('open');
 
-    document.getElementById('menu').classList.toggle('aberto');
+    document.getElementById('menu-responsivo').classList.toggle('aberto');
 });
 
